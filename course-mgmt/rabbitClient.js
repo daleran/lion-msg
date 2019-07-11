809000f85233157e5d015e4b257f3f7b5f48fe8f
daleran <daleran@gmail.com>
2019-07-11 20:40:18 +0900

    Added comments to the NodeJS App

--- course-mgmt/rabbitClient.js
+++ course-mgmt/rabbitClient.js
@@ -1,14 +1,21 @@
+// Class to encapsulate the amqplib package for connecting and listening to topics
 module.exports = class Rabbit {
+  // Connect to a specific host
   connect (hostname) {
+    // Wrap the ampq connection and channel creation in a promise
     return new Promise(function (resolve, reject) {
+      // Connect to the RabbitMQ service at the specified hostname
       require('amqplib').connect(hostname)
+      // Then create a chanel
         .then((connection) => {
           return connection.createChannel()
         })
+        // Then log the connection and resolve the promise with the channel
         .then((channel) => {
           console.log(`Connected to RabbitMQ on ${hostname}`)
           resolve(channel)
         })
+        // If there is an error, reject the promise and print an error
         .catch((error) => {
           console.error(`Error: Unable to connect to ${hostname}`)
           reject(error)
@@ -16,16 +23,24 @@ module.exports = class Rabbit {
     })
   }
 
-  listen (channel, exchange, topic, handler) {
+  // Listen for messages on the specified exhange and topic
+  listen (channel, exchange, topic, callback) {
+    // Wrap exchange and queue binding in a promise
     return new Promise(function (resolve, reject) {
+      // Assert a topic exchange exsists of the specified name
       channel.assertExchange(exchange, 'topic', { durable: false })
+      // Assert an anynomous queue exsists for the channnel
       channel.assertQueue('', { exclusive: true })
+      // Then bind the queue to the exchange and topic
         .then((q) => {
           console.log(`Listening for ${topic} on ${exchange}. Press CTRL+C to exit.`)
           channel.bindQueue(q.queue, exchange, topic)
-          channel.consume(q.queue, handler, { noAck: true })
+          // Consume any messages in the queue and pass them to the callback
+          channel.consume(q.queue, callback, { noAck: true })
         })
+        // Then resolve the promise as a success
         .then(() => { resolve() })
+        // Or reject the promise if there is an error and log it to the console
         .catch((error) => {
           console.error(`Error: Unable to listen on ${exchange}`)
           reject(error)
--- course-mgmt/studentController.js
+++ course-mgmt/studentController.js
@@ -1,5 +1,6 @@
 const Client = require('./rabbitClient')
 
+// Conver a Roster System student object to a Course Managment student object
 const toCourseManagmentStudent = (student) => {
   console.log('Converting to a Course Mangement Student')
   let converted = { }
@@ -11,11 +12,16 @@ const toCourseManagmentStudent = (student) => {
   return converted
 }
 
+// Callback to handle the message recieved from RabbitMQ
 const handleMessage = (message) => {
   try {
     console.log('Data recieved.')
+
+    // Parse the message into a JS object from JSON
     const rosterStudent = JSON.parse(message.content.toString())
     console.log(rosterStudent)
+
+    // Convert the Roster Stuent into a Course Management Student
     const courseMgmtStudent = toCourseManagmentStudent(rosterStudent)
     console.log(courseMgmtStudent)
   } catch (error) {
@@ -23,15 +29,21 @@ const handleMessage = (message) => {
   }
 }
 
+// Async function to listen for an traffic on the student.new topic
 const listenForNewStudents = async () => {
   try {
+    // Create a new RabbitMQ client
     const client = new Client()
+
+    // Connect to the localhose RabbitMQ service
     const channel = await client.connect('amqp://localhost')
+
+    // Listen on the student's exchange for student.new topics
     await client.listen(channel, 'students', 'student.new', handleMessage)
   } catch (error) {
     console.error('Error: Unable to set up client for new students.')
     process.exit(1)
   }
 }
-
+// Call the listen for new student function
 listenForNewStudents()
